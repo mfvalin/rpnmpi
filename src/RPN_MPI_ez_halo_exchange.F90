12d540ba09ab85f576c9b28e9c4557ae9c0fdd28
M. Valin <mfvalin@gmail.com>
2020-05-31 15:43:35 -0400

    simple halo exchanger + test program

--- src/RPN_MPI_ez_halo_exchange.F90
+++ src/RPN_MPI_ez_halo_exchange.F90
@@ -10,8 +10,8 @@
 ! * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 ! * Lesser General Public License for more details.
 
-#if ! defined(SELF_TEST)
-module RPN_MPI_halo_tcache
+module RPN_MPI_halo_cache
+  use ISO_C_BINDING
   implicit none
   include 'mpif.h'
   integer, parameter :: NTS = 9
@@ -20,138 +20,25 @@ module RPN_MPI_halo_tcache
 !   logical, save :: async    = .false.  ! if true, use fully asynchronous (isend, irecv)  method
   logical, save :: barrier  = .false.  ! if true, use a barrier between E-W and N-S exchanges
   integer, save :: nhalo = 0
-  integer, save :: gridcom = MPI_COMM_NULL
   integer, save :: rowcom  = MPI_COMM_NULL
   integer, save :: colcom  = MPI_COMM_NULL
   integer, save :: rankx, ranky, sizex, sizey
-end module RPN_MPI_halo_tcache
-
-function RPN_MPI_get_halo_timings(t,n) result(nt)
-  use RPN_MPI_halo_tcache
-  implicit none
-  integer, intent(IN) :: n
-  integer(kind=8), dimension(n), intent(OUT) :: t
-  integer :: nt
-  t(1:n) = -1
-  nt = -1
-  if(n < NTS) return
-  nt = nhalo
-  t(1:NTS) = ts
-  return
-end function RPN_MPI_get_halo_timings
-
-subroutine RPN_MPI_reset_halo_timings
-  use RPN_MPI_halo_tcache
-  implicit none
-  nhalo = 0
-  ts = 0
-  return
-end subroutine RPN_MPI_reset_halo_timings
-
-subroutine RPN_MPI_print_halo_timings
-  use RPN_MPI_halo_tcache
-  implicit none
-  integer(kind=8), dimension(:,:), allocatable :: allts
-  integer :: ier, ntot, i
-  integer(kind=8), dimension(NTS) :: tsavg, tsmax, tsmin
-
-  if(nhalo == 0) return   ! no stats collected
-
-  ntot = sizex*sizey
-  if(rankx == 0 .and. ranky == 0) then
-    allocate(allts(NTS,ntot))
-  else
-    allocate(allts(NTS,1))
-  endif
-  call MPI_Gather(ts, NTS, MPI_INTEGER8, allts, NTS, MPI_INTEGER8, 0, gridcom, ier)
-  do i = 1, NTS
-    tsavg(i) = sum(allts(i,:))
-    tsmax(i) = maxval(allts(i,:))
-    tsmin(i) = minval(allts(i,:))
-  enddo
-  tsavg = tsavg / ntot
-  if(rankx == 0 .and. ranky == 0) then
-    write(6,1)'nhalo, avg times =     nexch    peel-J    mesg-x    plug-J    peel-I    mesg-y    plug-I    exch-x    exch-y    barrier'
-    write(6,1)'nhalo, avg times =',nhalo,tsavg/nhalo
-    write(6,1)'min times        =          ',tsmin/nhalo
-    write(6,1)'max times        =          ',tsmax/nhalo
-    call flush(6)
-  endif
-  deallocate(allts)
-  return
-  
-1 format(a,10I10)
-end subroutine RPN_MPI_print_halo_timings
-
-! set information for halo exchange timings
-subroutine RPN_MPI_ez_halo_parms(grid, row, col, mode)
-  use RPN_MPI_halo_tcache
-  implicit none
-  integer, intent(IN) :: grid, row, col
-  character(len=*), intent(IN) :: mode   
-  integer :: ier
-  gridcom = grid  ! grid communicator
-  rowcom  = row   ! row communicator
-  colcom  = col   ! column communicator
-  call MPI_comm_size(rowcom, sizex, ier)  ! size of row
-  call MPI_comm_rank(rowcom, rankx, ier)  ! rank in row
-  call MPI_comm_size(colcom, sizey, ier)  ! size of column
-  call MPI_comm_rank(colcom, ranky, ier)  ! rank in column
-!   redblack = trim(mode) .eq. 'REDBLACK'
-!   async    = trim(mode) .eq. 'ASYNC'
-  barrier  = trim(mode) .eq. 'BARRIER'
-  if(rankx+ranky == 0) write(6,*) 'MODE = '//trim(mode)
-  return
-end subroutine RPN_MPI_ez_halo_parms
-
-! RED/BLACK method, only synchronous send and recv
-!   West -> East move
-!     EVEN PEs
-!       if not East PE send to odd PE at East 
-!       if not West PE recv from odd PE at West 
-!     ODD PEs
-!                      recv from even PE at West
-!       if not East PE send to even PE at East
-!   West <- East move
-!     EVEN PEs
-!       if not East PE recv from odd PE at East
-!       if not West PE send to odd PE at West
-!     ODD PEs
-!                      send to even PE at West
-!       if not East PE recv from even PE at East
-subroutine RPN_MPI_ez_halo_8(g,minx,maxx,miny,maxy,lni,lnj,nk,halox,haloy)
-  use ISO_C_BINDING
-  use RPN_MPI_halo_tcache
-  implicit none
-  integer, intent(IN)    :: minx,maxx,miny,maxy,lni,lnj,nk,halox,haloy
-  integer, intent(INOUT), dimension(2*minx-1:2*maxx,miny:maxy,nk) :: g
-  call RPN_MPI_quick_halo(g,2*minx-1,2*maxx,miny,maxy,2*lni,lnj,nk,2*halox,haloy,rowcom,colcom)
-end subroutine RPN_MPI_ez_halo_8
-
-subroutine RPN_MPI_quick_halo8(g,minx,maxx,miny,maxy,lni,lnj,nk,halox,haloy,row,col)
-  use ISO_C_BINDING
-  implicit none
-  integer, intent(IN)    :: minx,maxx,miny,maxy,lni,lnj,nk,halox,haloy,row,col
-  integer, intent(INOUT), dimension(2*minx-1:2*maxx,miny:maxy,nk) :: g
-
-  call RPN_MPI_quick_halo(g,2*minx-1,2*maxx,miny,maxy,2*lni,lnj,nk,2*halox,haloy,row,col)
-  return
-end subroutine RPN_MPI_quick_halo8
-
-subroutine RPN_MPI_ez_halo(g,minx,maxx,miny,maxy,lni,lnj,nk,halox,haloy)
-  use ISO_C_BINDING
-  use RPN_MPI_halo_tcache
-  implicit none
-  integer, intent(IN)    :: minx,maxx,miny,maxy,lni,lnj,nk,halox,haloy
-  integer, intent(INOUT), dimension(minx:maxx,miny:maxy,nk) :: g
-
-  call RPN_MPI_quick_halo(g,minx,maxx,miny,maxy,lni,lnj,nk,halox,haloy,rowcom,colcom)
-  return
-end subroutine RPN_MPI_ez_halo
-
-subroutine RPN_MPI_quick_halo(g,minx,maxx,miny,maxy,lni,lnj,nk,halox,haloy,row,col)
-  use ISO_C_BINDING
-  use RPN_MPI_halo_tcache
+contains
+! an interface will be needed to use RPN_MPI_quick_halo
+! the published interface will use the "void *" approach
+!
+!   type, BIND(C) :: address
+!     integer(C_INTPTR_T) :: a
+!   end type
+!   interface
+!     subroutine RPN_MPI_quick_halo(g,minx,maxx,miny,maxy,lni,lnj,nk,halox,haloy,row,col) BIND(C,name='RPN_MPI_quick_halo')
+!       import :: address
+!       integer, intent(IN)    :: minx,maxx,miny,maxy,lni,lnj,nk,halox,haloy,row,col
+!       type(address), intent(IN), value :: g
+!     end subroutine RPN_MPI_quick_halo
+!   end interface
+!
+subroutine RPN_MPI_quick_halo(g,minx,maxx,miny,maxy,lni,lnj,nk,halox,haloy,row,col) BIND(C,name='RPN_MPI_quick_halo')
   implicit none
   integer, intent(IN)    :: minx,maxx,miny,maxy,lni,lnj,nk,halox,haloy,row,col
   integer, intent(INOUT), dimension(minx:maxx,miny:maxy,nk) :: g
@@ -259,140 +146,134 @@ subroutine RPN_MPI_quick_halo(g,minx,maxx,miny,maxy,lni,lnj,nk,halox,haloy,row,c
   ts(9) = t(5) - t(4)
   return
 end subroutine RPN_MPI_quick_halo
-#endif
-#if defined(SELF_TEST)
-program test_ez_halo
-  use ISO_C_BINDING
+end module RPN_MPI_halo_cache
+
+function RPN_MPI_get_halo_timings(t,n) result(nt)
+  use RPN_MPI_halo_cache
   implicit none
-  include 'mpif.h'
-!   integer, parameter :: NI = 5
-!   integer, parameter :: NJ = 3
-!   integer, parameter :: NK = 1
-!   integer, parameter :: halox = 1
-!   integer, parameter :: haloy = 1
-  integer, parameter :: NXCH = 100
-  integer :: NI, NJ, NK, halox, haloy
-  integer, dimension(:,:,:), allocatable :: z
-  integer :: rankx, sizex, ranky, sizey, ier, petot, ranktot, errors
-  integer :: i, j, k, offx, offy, l, m, larg1, larg2, larg3, stat1, stat2, stat3, rowcomm, colcomm
-  integer :: ilo, ihi, jlo, jhi
-  character(len=128) :: argv1, argv2, argv3, mode
-  logical :: printit, redblack, yfirst, async, barrier
-  real(kind=8) :: t1, t2
-  real(kind=8), dimension(NXCH) :: txch
+  integer, intent(IN) :: n
+  integer(kind=8), dimension(n), intent(OUT) :: t
+  integer :: nt
+  t(1:n) = -1
+  nt = -1
+  if(n < NTS) return
+  nt = nhalo
+  t(1:NTS) = ts
+  return
+end function RPN_MPI_get_halo_timings
 
-  call MPI_Init(ier)
-  printit = .false.
-  call get_command_argument(1,argv1,larg1,stat1)
-  call get_command_argument(2,argv2,larg2,stat2)
-  call get_command_argument(3,argv3,larg3,stat3)
-  if(stat1 .ne. 0 .or. stat2 .ne. 0) goto 777
-  read(argv1,*,err=777)sizex,sizey
-  read(argv2,*,err=777)NI, NJ, NK, halox, haloy
-  mode = 'DEFAULT'
-  yfirst = .false.
-  if(stat3 .eq. 0) then
-    printit  = argv3(1:1) .eq. 't'  ! print arrays
-    redblack = argv3(2:2) .eq. 'r'  ! use red/black method
-    async    = argv3(2:2) .eq. 'a'  ! use async method
-    barrier  = argv3(2:2) .eq. 'b'  ! activate barrier between E-W and N-S
-    yfirst   = argv3(3:3) .eq. 'y'  ! y first, then x
+subroutine RPN_MPI_reset_halo_timings
+  use RPN_MPI_halo_cache
+  implicit none
+  nhalo = 0
+  ts = 0
+  return
+end subroutine RPN_MPI_reset_halo_timings
+
+subroutine RPN_MPI_print_halo_timings
+  use RPN_MPI_halo_cache
+  implicit none
+  integer(kind=8), dimension(:,:), allocatable :: alltsx, allts
+  integer :: ier, ntot, i
+  integer(kind=8), dimension(NTS) :: tsavg, tsmax, tsmin
+
+  if(nhalo == 0) return   ! no stats collected
+
+  ntot = sizex*sizey
+  if(rankx == 0) then
+    allocate(alltsx(NTS,sizex))   ! rank 0 in row will collect along x
+    if(ranky == 0) then
+      allocate(allts(NTS,ntot))   ! rank 0 in x and y will collect the full grid
+    else
+      allocate(allts(NTS,1))
+    endif
   else
-    argv3 = 't'  ! this will activate the rnak printout
+    allocate(alltsx(NTS,1))
+    allocate(allts(NTS,1))
   endif
-  if(redblack) mode = 'REDBLACK'
-  if(async)    mode = 'ASYNC'
-  if(barrier)  mode = 'BARRIER'
-  printit = printit .and. ni*nj < 30
-
-  allocate (z(1-halox:NI+halox,1-haloy:NJ+haloy,NK))
-
-  call MPI_comm_size(MPI_COMM_WORLD, petot, ier)
-  call MPI_comm_rank(MPI_COMM_WORLD, ranktot, ier)
-  if(sizex*sizey .ne. petot) goto 777
-  if(ranktot == 0) write(6,*)'redblack =',redblack
-  if(ranktot == 0) write(6,2)'sizex,sizey,NI,NJ,NK,halox,haloy=',sizex,sizey,NI,NJ,NK,halox,haloy
-  if(yfirst) then
-    rankx = ranktot / sizey
-    ranky = mod(ranktot,sizey)
-  else
-    ranky = ranktot / sizex
-    rankx = mod(ranktot,sizex)
+  call MPI_Gather(ts,     NTS,       MPI_INTEGER8, alltsx, NTS,       MPI_INTEGER8, 0, rowcom, ier)  ! collect along row
+  call MPI_Gather(alltsx, NTS*sizex, MPI_INTEGER8, allts,  NTS*sizex, MPI_INTEGER8, 0, colcom, ier)  ! collect all rows
+  do i = 1, NTS
+    tsavg(i) = sum(allts(i,:))
+    tsmax(i) = maxval(allts(i,:))
+    tsmin(i) = minval(allts(i,:))
+  enddo
+  tsavg = tsavg / ntot
+  if(rankx == 0 .and. ranky == 0) then
+    write(6,1)'nhalo, avg times =     nexch    peel-J    mesg-x    plug-J    peel-I    mesg-y    plug-I    exch-x    exch-y    barrier'
+    write(6,1)'nhalo, avg times =',nhalo,tsavg/nhalo
+    write(6,1)'min times        =          ',tsmin/nhalo
+    write(6,1)'max times        =          ',tsmax/nhalo
+    call flush(6)
   endif
-  do l=0,petot-1
-    if(ranktot .eq. l .and. argv3(1:1) .eq. 't') write(6,2)'ranktot, petot, rankx, ranky =',ranktot+1, petot, rankx, ranky
-    call MPI_Barrier(MPI_COMM_WORLD,ier)
-  enddo
-  call MPI_Comm_split(MPI_COMM_WORLD, ranky, ranktot, rowcomm,ier)
-  call MPI_Comm_split(MPI_COMM_WORLD, rankx, ranktot, colcomm,ier)
+  deallocate(allts)
+  return
+  
+1 format(a,10I10)
+end subroutine RPN_MPI_print_halo_timings
 
-  call RPN_MPI_ez_halo_parms(MPI_COMM_WORLD, rowcomm, colcomm, mode)
+! set information for halo exchange timings
+subroutine RPN_MPI_ez_halo_parms(grid, row, col, mode)
+  use RPN_MPI_halo_cache
+  implicit none
+  integer, intent(IN) :: grid, row, col
+  character(len=*), intent(IN) :: mode   
+  integer :: ier
+  rowcom  = row   ! row communicator
+  colcom  = col   ! column communicator
+  call MPI_comm_size(rowcom, sizex, ier)  ! size of row
+  call MPI_comm_rank(rowcom, rankx, ier)  ! rank in row
+  call MPI_comm_size(colcom, sizey, ier)  ! size of column
+  call MPI_comm_rank(colcom, ranky, ier)  ! rank in column
+!   redblack = trim(mode) .eq. 'REDBLACK'
+!   async    = trim(mode) .eq. 'ASYNC'
+  barrier  = trim(mode) .eq. 'BARRIER'
+  if(rankx+ranky == 0) write(6,*) 'MODE = '//trim(mode)
+  return
+end subroutine RPN_MPI_ez_halo_parms
 
-  offy = ranky * NJ
-  offx = rankx * NI
-  z = 0
-  do k=1,NK
-  do j=1,NJ
-  do i=1,NI
-    z(i,j,k) = 1000000*mod(i+offx,256) + 1000*mod(j+offy,256) + k
-  enddo
-  enddo
-  enddo
-  call MPI_Barrier(MPI_COMM_WORLD,ier)
-  call RPN_MPI_ez_halo(z,1-halox,NI+halox,1-haloy,NJ+haloy,NI,NJ,NK,halox,haloy)
-  call RPN_MPI_reset_halo_timings
-  call MPI_Barrier(MPI_COMM_WORLD,ier)
+! RED/BLACK method uses only synchronous send and recv
+!   West -> East move
+!     EVEN PEs
+!       if not East PE send to odd PE at East 
+!       if not West PE recv from odd PE at West 
+!     ODD PEs
+!                      recv from even PE at West
+!       if not East PE send to even PE at East
+!   West <- East move
+!     EVEN PEs
+!       if not East PE recv from odd PE at East
+!       if not West PE send to odd PE at West
+!     ODD PEs
+!                      send to even PE at West
+!       if not East PE recv from even PE at East
+subroutine RPN_MPI_ez_halo_8(g,minx,maxx,miny,maxy,lni,lnj,nk,halox,haloy)
+  use ISO_C_BINDING
+  use RPN_MPI_halo_cache
+  implicit none
+  integer, intent(IN)    :: minx,maxx,miny,maxy,lni,lnj,nk,halox,haloy
+  integer, intent(INOUT), dimension(2*minx-1:2*maxx,miny:maxy,nk) :: g
+  call RPN_MPI_quick_halo(g,2*minx-1,2*maxx,miny,maxy,2*lni,lnj,nk,2*halox,haloy,rowcom,colcom)
+end subroutine RPN_MPI_ez_halo_8
 
-  do i = 1, NXCH
-    call MPI_Barrier(MPI_COMM_WORLD,ier)
-    t1 = MPI_Wtime()
-    call RPN_MPI_ez_halo(z,1-halox,NI+halox,1-haloy,NJ+haloy,NI,NJ,NK,halox,haloy)
-    txch(i) = MPI_Wtime() - t1
-  enddo
-  txch = txch * 1000000   ! convert to microseconds
-  call MPI_Barrier(MPI_COMM_WORLD,ier)
+subroutine RPN_MPI_quick_halo8(g,minx,maxx,miny,maxy,lni,lnj,nk,halox,haloy,row,col)
+  use ISO_C_BINDING
+  use RPN_MPI_halo_cache
+  implicit none
+  integer, intent(IN)    :: minx,maxx,miny,maxy,lni,lnj,nk,halox,haloy,row,col
+  integer, intent(INOUT), dimension(2*minx-1:2*maxx,miny:maxy,nk) :: g
 
-  call RPN_MPI_print_halo_timings
-!   do i = 0, petot-1, max(petot/16,1)
-!     if(ranktot .eq. i) call RPN_MPI_print_halo_timings
-!   enddo
-  call MPI_Barrier(MPI_COMM_WORLD,ier)
-  if(printit) then
-    do m = sizey-1, 0, -1
-    do l = sizex-1, 0, -1
-      if(ranktot .eq. l+m*sizex)then
-	write(6,*)"PE(",rankx,',',ranky,')'
-	do j=NJ+haloy,1-haloy,-1
-	  print 1,z(:,j,1)
-	enddo
-      endif
-      call MPI_Barrier(MPI_COMM_WORLD,ier)
-    enddo
-    enddo
-  endif
-  errors = 0
-  ilo = 1 - halox
-  jlo = 1 - haloy
-  ihi = NI + halox
-  jhi = NJ + haloy
-  if(ranky .eq. 0) jlo = 1         ! south PE
-  if(ranky .eq. sizey-1) jhi = NJ  ! north PE
-  if(rankx .eq. 0) ilo = 1         ! west PE
-  if(rankx .eq. sizex-1) ihi = NI  ! east PE
-  do k = 1, nk
-  do j = jlo, jhi
-  do i = ilo, ihi
-    if(z(i,j,k) .ne. 1000000*mod(i+offx,256) + 1000*mod(j+offy,256) + k) errors = errors + 1
-  enddo
-  enddo
-  enddo
-  call flush(6)
-  call MPI_Barrier(MPI_COMM_WORLD,ier)
-  if(ranktot .eq. 0) write(6,*)'avg/min/max per exchange =',sum(txch)/NXCH,minval(txch),maxval(txch),' microseconds'
-  if(errors .ne. 0) write(6,*)'rank, ERRORS =',ranktot, errors
-777 continue
-  call MPI_Finalize(ier)
-1 format(20I10.9)
-2 format(A,20i6)
-end program
-#endif
+  call RPN_MPI_quick_halo(g,2*minx-1,2*maxx,miny,maxy,2*lni,lnj,nk,2*halox,haloy,row,col)
+  return
+end subroutine RPN_MPI_quick_halo8
+
+subroutine RPN_MPI_ez_halo(g,minx,maxx,miny,maxy,lni,lnj,nk,halox,haloy)
+  use ISO_C_BINDING
+  use RPN_MPI_halo_cache
+  implicit none
+  integer, intent(IN)    :: minx,maxx,miny,maxy,lni,lnj,nk,halox,haloy
+  integer, intent(INOUT), dimension(minx:maxx,miny:maxy,nk) :: g
+
+  call RPN_MPI_quick_halo(g,minx,maxx,miny,maxy,lni,lnj,nk,halox,haloy,rowcom,colcom)
+  return
+end subroutine RPN_MPI_ez_halo
---
+++ src/TEST_103.F90
@@ -0,0 +1,160 @@
+subroutine rpn_mpi_test_103
+!
+! simple, standalone test for RPN_MPI_ez_halo_parms
+! does not use the full RPN_MPI setup
+!
+  use ISO_C_BINDING
+  implicit none
+  include 'mpif.h'
+! following include need to get rpnmpi_loc
+  include 'RPN_MPI.inc'
+  interface
+    subroutine RPN_MPI_quick_halo(g,minx,maxx,miny,maxy,lni,lnj,nk,halox,haloy,row,col) BIND(C,name='RPN_MPI_quick_halo')
+      import :: rpnmpi_loc, C_INTPTR_T
+      integer, intent(IN)    :: minx,maxx,miny,maxy,lni,lnj,nk,halox,haloy,row,col
+      type(rpnmpi_loc), intent(IN), value :: g
+    end subroutine RPN_MPI_quick_halo
+  end interface
+  type(rpnmpi_loc) :: p
+  integer, parameter :: NXCH = 100
+  integer :: NI, NJ, NK, halox, haloy
+  integer, dimension(:,:,:), allocatable :: z
+  integer :: rankx, sizex, ranky, sizey, ier, petot, ranktot, errors
+  integer :: i, j, k, offx, offy, l, m, larg1, larg2, larg3, stat1, stat2, stat3, rowcomm, colcomm
+  integer :: ilo, ihi, jlo, jhi
+  character(len=128) :: argv1, argv2, argv3, mode
+  logical :: printit, redblack, yfirst, async, barrier
+  real(kind=8) :: t1, t2
+  real(kind=8), dimension(NXCH) :: txch
+
+  call MPI_Init(ier)
+  printit = .false.
+  call get_command_argument(1,argv1,larg1,stat1)
+  call get_command_argument(2,argv2,larg2,stat2)
+  call get_command_argument(3,argv3,larg3,stat3)
+  if(stat1 .ne. 0 .or. stat2 .ne. 0) goto 777
+  read(argv1,*,err=777)sizex,sizey
+  read(argv2,*,err=777)NI, NJ, NK, halox, haloy
+  mode = 'DEFAULT'
+  yfirst = .false.
+  if(stat3 .eq. 0) then
+    printit  = argv3(1:1) .eq. 't'  ! print arrays
+!     redblack = argv3(2:2) .eq. 'r'  ! use red/black method
+!     async    = argv3(2:2) .eq. 'a'  ! use async method
+    barrier  = argv3(2:2) .eq. 'b'  ! activate barrier between E-W and N-S
+    yfirst   = argv3(3:3) .eq. 'y'  ! y first, then x
+  else
+    argv3 = 't'  ! this will activate the rnak printout
+  endif
+!   if(redblack) mode = 'REDBLACK'
+!   if(async)    mode = 'ASYNC'
+  if(barrier)  mode = 'BARRIER'
+  printit = printit .and. ni*nj < 30
+
+  allocate (z(1-halox:NI+halox,1-haloy:NJ+haloy,NK))
+!
+! split MPI_COMM_WORLD into row and column
+!
+  call MPI_comm_size(MPI_COMM_WORLD, petot, ier)
+  call MPI_comm_rank(MPI_COMM_WORLD, ranktot, ier)
+  if(sizex*sizey .ne. petot) goto 777
+  if(ranktot == 0) write(6,*)'redblack =',redblack
+  if(ranktot == 0) write(6,2)'sizex,sizey,NI,NJ,NK,halox,haloy=',sizex,sizey,NI,NJ,NK,halox,haloy
+  if(yfirst) then
+    rankx = ranktot / sizey
+    ranky = mod(ranktot,sizey)
+  else
+    ranky = ranktot / sizex
+    rankx = mod(ranktot,sizex)
+  endif
+  do l=0,petot-1
+    if(ranktot .eq. l .and. argv3(1:1) .eq. 't') write(6,2)'ranktot, petot, rankx, ranky =',ranktot+1, petot, rankx, ranky
+    call MPI_Barrier(MPI_COMM_WORLD,ier)
+  enddo
+  call MPI_Comm_split(MPI_COMM_WORLD, ranky, ranktot, rowcomm,ier)
+  call MPI_Comm_split(MPI_COMM_WORLD, rankx, ranktot, colcomm,ier)
+! set halo exchange timing parameters
+  call RPN_MPI_ez_halo_parms(MPI_COMM_WORLD, rowcomm, colcomm, mode)
+
+  offy = ranky * NJ
+  offx = rankx * NI
+  z = 0
+  do k=1,NK
+  do j=1,NJ
+  do i=1,NI
+    z(i,j,k) = 1000000*mod(i+offx,256) + 1000*mod(j+offy,256) + k
+  enddo
+  enddo
+  enddo
+  call MPI_Barrier(MPI_COMM_WORLD,ier)
+!
+  p%a = loc(z)                             ! address of array subject to halo exchange
+!
+! if we use
+!      include 'RPN_MPI_mpi_definitions.inc'
+!      call RPN_MPI_init(.......) 
+!      type(mpi_layout) :: l
+!      call RPN_MPI_get_mpi_layout(l, ierr)
+! then we can use
+!      rowcomm = p%comm%grid%row
+!      colcomm = p%comm%grid%column
+!
+  call RPN_MPI_quick_halo(p,1-halox,NI+halox,1-haloy,NJ+haloy,NI,NJ,NK,halox,haloy,rowcomm,colcomm)
+  call RPN_MPI_reset_halo_timings          ! ignore timings for first call
+  call MPI_Barrier(MPI_COMM_WORLD,ier)     ! full sync
+
+  do i = 1, NXCH
+    call MPI_Barrier(MPI_COMM_WORLD,ier)
+    t1 = MPI_Wtime()
+    p%a = loc(z)
+    call RPN_MPI_quick_halo(p,1-halox,NI+halox,1-haloy,NJ+haloy,NI,NJ,NK,halox,haloy,rowcomm,colcomm)
+    txch(i) = MPI_Wtime() - t1
+  enddo
+  txch = txch * 1000000   ! convert into microseconds
+  call MPI_Barrier(MPI_COMM_WORLD,ier)
+
+  call RPN_MPI_print_halo_timings
+
+  call MPI_Barrier(MPI_COMM_WORLD,ier)
+  if(printit) then                       ! if we are printing arrays after exchange
+    do m = sizey-1, 0, -1
+    do l = sizex-1, 0, -1
+      if(ranktot .eq. l+m*sizex)then
+	write(6,*)"PE(",rankx,',',ranky,')'
+	do j=NJ+haloy,1-haloy,-1
+	  print 1,z(:,j,1)
+	enddo
+      endif
+      call MPI_Barrier(MPI_COMM_WORLD,ier)
+    enddo
+    enddo
+  endif
+!
+! error analysis
+!
+  errors = 0
+  ilo = 1 - halox
+  jlo = 1 - haloy
+  ihi = NI + halox
+  jhi = NJ + haloy
+  if(ranky .eq. 0) jlo = 1         ! south PE
+  if(ranky .eq. sizey-1) jhi = NJ  ! north PE
+  if(rankx .eq. 0) ilo = 1         ! west PE
+  if(rankx .eq. sizex-1) ihi = NI  ! east PE
+  do k = 1, nk
+  do j = jlo, jhi
+  do i = ilo, ihi
+    if(z(i,j,k) .ne. 1000000*mod(i+offx,256) + 1000*mod(j+offy,256) + k) errors = errors + 1
+  enddo
+  enddo
+  enddo
+  call flush(6)
+  call MPI_Barrier(MPI_COMM_WORLD,ier)
+  if(ranktot .eq. 0) write(6,*)'avg/min/max per exchange =',sum(txch)/NXCH,minval(txch),maxval(txch),' microseconds'
+  if(errors .ne. 0)  write(6,*)'rank, ERRORS =',ranktot, errors
+!   write(6,*)'rank, ERRORS =',ranktot, errors
+777 continue
+  call MPI_Finalize(ier)
+1 format(20I10.9)
+2 format(A,20i6)
+end subroutine rpn_mpi_test_103
