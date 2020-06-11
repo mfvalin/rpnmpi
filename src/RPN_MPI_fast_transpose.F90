e7e6b85b2cc53dadb336600569eb0d5dae454b7b
M. Valin <mfvalin@gmail.com>
2020-06-11 04:02:02 -0400

    transpose along x and y passses first tests

--- src/RPN_MPI_fast_transpose.F90
+++ src/RPN_MPI_fast_transpose.F90
@@ -22,10 +22,6 @@
 !
 !                         ===    with x and y transposes  ===
 !
-!                         dimension(mini:maxi, minj:maxj, gnk) [ or (lnix,lnjy,gnk) ]
-!                         |                                  ^
-!                         |                                  |
-!                         v                                  |
 !                         dimension(lnix,      lnjy,      gnk)
 !                         |                                  ^
 !                         |                                  |
@@ -88,15 +84,29 @@ module RPN_MPI_fast_transpose_cache
   integer, dimension(MAX_PEX), save :: sendcount, senddispl, recvcount, recvdispl, countnk
   integer, save :: nk = 0
   integer, save :: npex = 0
-  integer, save :: myrank = -1
+  integer, save :: npey = 0
+  integer, save :: rankx = -1
   integer, save :: rowcom = MPI_COMM_NULL
+  integer, save :: colcom = MPI_COMM_NULL
 end module RPN_MPI_fast_transpose_cache
+!
+! transpose along x
+!
+! in the original array a given PE has 
+! all slices(consecutive in memory) along z (all k indices, gnk values)
+! one slice along y (length lnjy)
+! one slice along x (length lnix)
+!
+! in the transposed array, a given PE has
+! one slice along z (length lnkx)
+! one slice along y (length lnjy)
+! all slices along x (all i indices, gni values)
 !
 ! forward transpose (forward == .true.)
 ! z         : original array, dimension(lnix,lnjy,gnk)
 ! zt        : transposed arrray, dimension(lnix,lnjy,lnkx,npex)
-! lnix      : number of local points along x (assumed to be IDENTICAL on ALL PEs)
-! lniy      : number of local points along y (assumed to be IDENTICAL on ALL PEs)
+! lnix      : number of local points along x (assumed to be IDENTICAL on ALL PEs in row)
+! lnjy      : number of local points along y (assumed to be IDENTICAL on ALL PEs in row)
 ! gnk       : total number of levels
 ! lnkx      : local number of levels in transposed array zt (the SUM of lnkx MUST be equal to gnk)
 ! row_comm  : communicator for this transpose
@@ -112,24 +122,27 @@ end module RPN_MPI_fast_transpose_cache
   integer, intent(IN) :: row_comm                               !InTf!
 ! little white lie in interface, z, zt are advertised as addresses passed by value
 !! type(RPN_MPI_Loc), intent(IN), value :: z, zt                !InTf!
-  integer, dimension(lnix,lnjy,gnk), intent(INOUT)    :: z      ! NO HALO in local arrays 
+  integer, dimension(lnix,lnjy,gnk), intent(INOUT)    :: z      ! NO HALO in arrays 
   integer, dimension(lnix,lnjy,lnkx,*), intent(INOUT) :: zt     ! last dimension is npex
   integer, intent(OUT) :: ierr                                  !InTf!
 
   integer :: n
+  integer :: i, j, k, m
 
   ierr = MPI_ERROR
   if(npex == 0 .or. row_comm .ne. rowcom .or. nk .ne. gnk) then
+
     rowcom = row_comm
-    call MPI_Comm_rank(rowcom, myrank, ierr)
+    call MPI_Comm_rank(rowcom, rankx, ierr)
     call MPI_Comm_size(rowcom, npex, ierr)
     if(npex > MAX_PEX) goto 111                        ! ERROR, tables are too small
     call MPI_Allgather(lnkx, 1, MPI_INTEGER, countnk, 1, MPI_INTEGER, rowcom, ierr) ! get lnk counts
     if(sum(countnk(1:npex)) .ne. gnk) goto 222         ! ERROR, missing or extra levels
   endif
-
+! write(0,*) 'sendcount = ',sendcount(1:npex)
+! write(0,*) 'lnix, lnjy, gnk, lnkx =',lnix, lnjy, gnk, lnkx
   if(forward) then                                     ! z -> zt
-    sendcount(1:npex) = countnk(1:nk) * lnix * lnjy    ! may be zero for some PEs
+    sendcount(1:npex) = countnk(1:npex) * lnix * lnjy    ! may be zero for some PEs
     recvcount(1:npex) = lnkx * lnix * lnjy             ! may be zero for this PE
     senddispl(1) = 0
     recvdispl(1) = 0
@@ -137,11 +150,23 @@ end module RPN_MPI_fast_transpose_cache
       senddispl(n) = sendcount(n-1) + senddispl(n-1)
       recvdispl(n) = recvcount(n-1) + recvdispl(n-1)
     enddo
+! write(0,*) 'sendcount = ',sendcount(1:npex)
+! write(0,*) 'senddispl = ',senddispl(1:npex)
+! write(0,*) 'recvcount = ',recvcount(1:npex)
+! write(0,*) 'recvdispl = ',recvdispl(1:npex)
+! write(0,*) 'z'
+! do j = lnjy,1,-1
+!   write(0,2) (z(1:lnix,j,k),k=1,gnk)
+! enddo
     call MPI_Alltoallv(z , sendcount, senddispl, MPI_INTEGER,               &
                        zt, recvcount, recvdispl, MPI_INTEGER, rowcom, ierr)
+! write(0,*) 'zt'
+! do j = lnjy,1,-1
+!   write(0,2) (zt(1:lnix,j,1,m),m=1,npex)
+! enddo
   else                                                 ! zt -> z
     sendcount(1:npex) = lnkx * lnix * lnjy             ! may be zero for this PE
-    recvcount(1:npex) = countnk(1:nk) * lnix * lnjy    ! may be zero for some PEs
+    recvcount(1:npex) = countnk(1:npex) * lnix * lnjy    ! may be zero for some PEs
     do n = 2, npex
       senddispl(n) = sendcount(n-1) + senddispl(n-1)
       recvdispl(n) = recvcount(n-1) + recvdispl(n-1)
@@ -157,7 +182,71 @@ end module RPN_MPI_fast_transpose_cache
 222 continue
   write(0,*) 'ERROR: the sum of lnk() does not match gnk'
   goto 1
+2 format(30I10.9)
  end subroutine RPN_MPI_fast_transpose_x !InTf!
+!
+! transpose along y, the original and transposed arrays have the same dimensions
+!
+! in the original array a given PE has
+!
+! all slices along x (all i indices, gni values)
+! one slice along y (length lnjy)
+! one slice along z (length lnkx)
+!
+! in the transposed array, a given PE has
+! one slice along x (length lniy)
+! all slices along y (all j indices, gnj values)
+! one slice along z (length lnkx)
+!
+! forward transpose (forward == .true.)
+! z         : original array, dimension(lniy,lnjy,lnkx,npey)
+! zt        : transposed arrray, dimension(lniy,lnjy,lnkx,npey)
+! lniy      : number of local points along x (assumed to be IDENTICAL on ALL PEs in column)
+! lnjy      : number of local points along y (assumed to be IDENTICAL on ALL PEs in column)
+! lnkx      : local number of levels in transposed array zt (the SUM of lnkx MUST be equal to gnk)
+! col_comm  : communicator for this transpose
+! ierr      : error flag, same as MPI
+!
+ subroutine RPN_MPI_fast_transpose_y(z, zt, forward, lniy, lnjy, lnkx, col_comm, ierr) !InTf!
+  use ISO_C_BINDING
+  use RPN_MPI_fast_transpose_cache
+  implicit none
+!! import :: RPN_MPI_Loc                                        !InTf!
+  logical, intent(IN) :: forward                                !InTf!
+  integer, intent(IN) :: lniy, lnjy, lnkx                  !InTf!
+  integer, intent(IN) :: col_comm                               !InTf!
+! little white lie in interface, z, zt are advertised as addresses passed by value
+!! type(RPN_MPI_Loc), intent(IN), value :: z, zt                !InTf!
+  integer, dimension(lniy,lnjy,lnkx,*), intent(INOUT) :: z      ! NO HALO in arrays 
+  integer, dimension(lniy,lnjy,lnkx,*), intent(INOUT) :: zt     ! last dimension is npey
+  integer, intent(OUT) :: ierr                                  !InTf!
+
+  integer :: n
+  integer :: i, j, k, m
+
+  ierr = MPI_ERROR
+  if(npey == 0 .or. col_comm .ne. colcom) then
+    colcom = col_comm
+    call MPI_Comm_size(colcom, npey, ierr)
+  endif
+
+  if(forward) then                                     ! z -> zt
+    call MPI_Alltoall(z , lniy*lnjy*lnkx, MPI_INTEGER,               &
+                      zt, lniy*lnjy*lnkx, MPI_INTEGER, colcom, ierr)
+  else                                                 ! zt -> z
+    call MPI_Alltoall(zt, lniy*lnjy*lnkx, MPI_INTEGER,               &
+                      z , lniy*lnjy*lnkx, MPI_INTEGER, colcom, ierr)
+  endif
+
+1 return
+111 continue
+  write(0,*) 'ERROR: more than',MAX_PEX,' levels'
+  goto 1
+222 continue
+  write(0,*) 'ERROR: the sum of lnk() does not match gnk'
+  goto 1
+2 format(30I10.9)
+ end subroutine RPN_MPI_fast_transpose_y !InTf!
 
 
 
--- src/TEST_104.F90
+++ src/TEST_104.F90
@@ -10,11 +10,15 @@ subroutine rpn_mpi_test_104
 
   integer :: ier, npe, rank
   type(RPN_MPI_mpi_definitions_raw) :: dr
-  integer :: i, j, k, larg1, larg2, larg3, stat1, stat2, stat3, lni, lnj, lnk, gni, gnj, gnk
-  character(len=128) :: argv1, argv2, argv3, mode
-  integer :: lnimax, lnjmax, lnkmax
-  integer, dimension(:,:,:), allocatable   :: z
-  integer, dimension(:,:,:,:), allocatable :: zt
+  integer :: i, j, k, n, larg, stat, lni, lnj, lnk, gni, gnj, gnk, npex, npey, ranki, rankj
+  integer :: i0, j0, k0, i0y, lniy, lnimaxy
+  character(len=128) :: argv(5), mode
+  integer :: lnimax, lnjmax, lnkmax, errors
+  integer(kind=8), dimension(:,:,:), allocatable   :: z, z2        ! original and restored original
+  integer(kind=8), dimension(:), allocatable       :: zgi, zgj     ! full row, full column
+  integer(kind=8), dimension(:,:,:,:), allocatable :: zt, zy, zty
+  integer :: row_comm, col_comm
+  integer :: myrow, mycol
 
   call MPI_Init(ier)
 
@@ -22,35 +26,152 @@ subroutine rpn_mpi_test_104
   call MPI_Comm_size(dr%MPI_COMM_WORLD, npe, ier)
   call MPI_Comm_rank(dr%MPI_COMM_WORLD, rank, ier)
 
-  call get_command_argument(1,argv1,larg1,stat1)
-  call get_command_argument(2,argv2,larg2,stat2)
-  call get_command_argument(3,argv3,larg3,stat3)
-  if(stat1 .ne. 0 .or. stat2 .ne. 0 .or. stat3 .ne. 0) goto 777
+  do i=1,5
+    call get_command_argument(i,argv(i),larg,stat)
+    if(stat .ne. 0) goto 777
+  enddo
   write(0,*) 'I am PE',rank+1,' of',npe
-  read(argv1,*,err=777) gni
-  read(argv2,*,err=777) gnj
-  read(argv3,*,err=777) gnk
-  lnimax = (gni + npe -1) / npe
-  lnjmax = (gnj + npe -1) / npe
-  lnkmax = (gnk + npe -1) / npe
-  lni = min(lnimax, gni - (lnimax * rank))
+  read(argv(1),*,err=777) gni
+  read(argv(2),*,err=777) gnj
+  read(argv(3),*,err=777) gnk
+  read(argv(4),*,err=777) npex
+  read(argv(5),*,err=777) npey
+  if(npex*npey .ne. npe) then
+    write(0,*) 'got',npe,' PEs, expected',npex*npey
+    goto 777
+  endif
+
+  ranki = mod(rank,npex)
+  mycol = ranki
+  call MPI_Comm_split(dr%MPI_COMM_WORLD, mycol, rank, col_comm, ier)
+  rankj = rank / npex
+  myrow = rankj
+  call MPI_Comm_split(dr%MPI_COMM_WORLD, myrow, rank, row_comm, ier)
+  lnimax = (gni + npex -1) / npex      ! gni distributed over x
+  i0 = lnimax * ranki
+  lnimaxy = (gni + npey -1) / npey     ! gni distributed over y
+  i0y = lnimaxy * rankj
+  lnjmax = (gnj + npey -1) / npey      ! gnj distributed over y
+  j0 = lnjmax * rankj
+  lnkmax = (gnk + npex -1) / npex      ! gnk distributed over x
+  k0 = lnkmax * ranki
+  lni = min(lnimax, gni - (lnimax * ranki))
   lni = max(0,lni)
-  lnj = min(lnjmax, gnj - (lnjmax * rank))
+  lniy = min(lnimaxy, gni - (lnimaxy * ranki))
+  lniy = max(0,lniy)
+  lnj = min(lnjmax, gnj - (lnjmax * rankj))
   lnj = max(0,lnj)
-  lnk = min(lnkmax, gnk - (lnkmax * rank))
+  lnk = min(lnkmax, gnk - (lnkmax * ranki))
   lnk = max(0,lnk)
-  lnjmax = (gnj + npe -1) / npe
-  lnkmax = (gnk + npe -1) / npe
-  write(0,*) gni, gnj, gnk, lni, lnj, lnk
-  allocate(z(lni,lnj,gnk))
-  allocate(zt(lni,lnj,max(1,lnk),npe))
-  z = 0
-  zt = 0
-  write(0,*) sum(z), sum(zt)
+  write(0,*) 'gni, gnj, gnk, lni, lnj, lnk =',gni, gnj, gnk, lni, lnj, lnk
+  write(0,*) 'ranki, rankj, lnimax, lnimaxy =',ranki, rankj, lnimax, lnimaxy
+
+  allocate( z(lnimax,lnjmax,gnk))                 ! original array
+  allocate(z2(lnimax,lnjmax,gnk))                 ! restored original array
+  allocate(zt(lnimax,lnjmax,max(1,lnk),npex))     ! after x transpose
+  allocate( zgi(lnimax*npex))                         ! a full row, used to reshape zt into zy, and zy into zt
+  allocate( zy(lnimaxy,lnjmax,max(1,lnk),npey))   ! before y transpose, after inverse y transpose
+  allocate(zty(lnimaxy,lnjmax,max(1,lnk),npey))   ! after y transpose
+  allocate(zgj(npey*lnjmax))                      ! a full column
+
+  do k = 1, gnk
+  do j = 1, lnj
+  do i = 1, lni
+    z(i,j,k) = (i0+i)*1000000 + (j0+j)*1000 + k
+  enddo
+  do i = lni+1, lnimax
+    z(i,j,k) = 0
+  enddo
+  enddo
+  enddo
+  if(gni <= 16) then
+    write(0,*) 'before x transpose'
+    do j = lnj,1,-1
+      write(0,2)(z(1:lnimax,j,k),k=1,gnk)
+    enddo
+  endif
+  call RPN_MPI_fast_transpose_x(rpn_mpi_loc(loc(z)), rpn_mpi_loc(loc(zt)), .true., lnimax*2, lnjmax, gnk, lnk, row_comm, ier)
+  if(gni <= 16) then
+    write(0,*) 'after x transpose'
+    do j = lnj,1,-1
+      write(0,2)(zt(1:lnimax,j,1,n), n=1,npex)
+    enddo
+  endif
+
+  ! reshape (lnix,lnjy,lnkx,npex) -> (lniy,lnjy,lnkx,npey)
+  do k = 1,lnk
+  do j = 1,lnjmax
+    do n = 0,npex-1    ! (1:lnix,i,j,1:npex) -> (gni)
+    do i = 1,lnimax
+      zgi (n*lnimax+i) = zt(i,j,k,n+1)   ! gather full row
+    enddo
+    enddo
+    do n = 0,npey-1    ! (gni) -> (1:lniy,i,j,1:npey) 
+    do i = 1,lnimaxy
+      zy(i,j,k,n+1) = zgi (n*lnimaxy+i)  ! scatter full row
+    enddo
+    enddo
+  enddo
+  enddo
+
+  if(gni <= 16) then
+    write(0,*) 'before y transpose'
+    do j = lnj,1,-1
+    enddo
+  endif
+  call RPN_MPI_fast_transpose_y(rpn_mpi_loc(loc(zy)), rpn_mpi_loc(loc(zty)), .true., lnimaxy*2, lnjmax, lnk, col_comm, ier)
+  if(gni <= 16) then
+    write(0,*) 'after y transpose'
+    do j = lnj,1,-1
+    enddo
+  endif
+  call RPN_MPI_fast_transpose_y(rpn_mpi_loc(loc(zy)), rpn_mpi_loc(loc(zty)), .false., lnimaxy*2, lnjmax, lnk, col_comm, ier)
+  if(gni <= 16) then
+    write(0,*) 'after inverse y transpose'
+    do j = lnj,1,-1
+    enddo
+  endif
+
+  ! reshape (lniy,lnjy,lnkx,npey) -> (lnix,lnjy,lnkx,npex)
+  do k = 1,lnk
+  do j = 1,lnjmax
+    do n = 0,npey-1    ! (1:lniy,j,k,1:npey) -> (gni)
+    do i = 1,lnimaxy
+      zgi (n*lnimaxy+i) = zy(i,j,k,n+1)   ! gather full row
+    enddo
+    enddo
+    do n = 0,npex-1    ! (gni) -> (1:lnix,j,k,1:npex) 
+    do i = 1,lnimax
+      zt(i,j,k,n+1) = zgi (n*lnimax+i)    ! scatter full row
+    enddo
+    enddo
+  enddo
+  enddo
+
+  z2 = 0
+  call RPN_MPI_fast_transpose_x(rpn_mpi_loc(loc(z2)), rpn_mpi_loc(loc(zt)), .false., lnimax*2, lnjmax, gnk, lnk, row_comm, ier)
+  if(gni <= 16) then
+    write(0,*) 'after inverse x transpose'
+    do j = lnj,1,-1
+      write(0,2)(z2(1:lnimax,j,k),k=1,gnk)
+    enddo
+  endif
+
+  errors = 0
+  do k = 1, gnk
+  do j = 1, lnj
+  do i = 1, lni
+    if(z(i,j,k) .ne. z2(i,j,k)) errors = errors + 1
+  enddo
+  enddo
+  enddo
+  write(0,*) 'errors =',errors
+
 1 call MPI_Finalize(ier)
   return
 
 777 continue
   write(0,*) 'ERROR in arguments'
   goto 1
+2 format(30I10.9)
 end subroutine rpn_mpi_test_104
